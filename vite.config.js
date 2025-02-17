00a1b6846cd27c0422c9ec49e454d7957809f55f
DeivyAyala <107268555+DeivyAyala@users.noreply.github.com>
2025-02-17 11:59:58 -0500

    Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AppPokemon/', // Asegúrate de que coincida con el nombre de tu repo en GitHub
 })
